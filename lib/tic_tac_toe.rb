691a4d78b6b7323c5892b486be3f379eedf34e2e
heruku <uku.taht@gmail.com>
2014-06-16 13:39:29 +0100

    require all the files

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,5 @@
 require_relative 'tic_tac_toe/core/board'
 require_relative 'tic_tac_toe/core/computer_player'
+require_relative 'tic_tac_toe/core/human_player'
 require_relative 'tic_tac_toe/core/negamax_cache'
 require_relative 'tic_tac_toe/core/game'
